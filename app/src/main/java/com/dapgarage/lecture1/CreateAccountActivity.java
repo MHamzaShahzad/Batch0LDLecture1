e4484b74562b065c1317995c69fa913cd1d5890c
MHamzaShahzad <hamzashahzad604@gmail.com>
2020-07-14 21:23:07 +0500

    Add user info in firebase realtime database when registers.

--- app/src/main/java/com/dapgarage/lecture1/CreateAccountActivity.java
+++ app/src/main/java/com/dapgarage/lecture1/CreateAccountActivity.java
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.dapgarage.lecture1.models.FirebaseDatabaseUser;
+import com.dapgarage.lecture1.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,7 +31,27 @@ public class CreateAccountActivity extends AppCompatActivity {
     @BindView(R.id.registerPasswordInput)
     EditText registerPasswordInput;
 
+    @BindView(R.id.registerFirstNameInput)
+    EditText registerFirstNameInput;
+
+    @BindView(R.id.registerLastNameInput)
+    EditText registerLastNameInput;
+
+    @BindView(R.id.registerPhoneNumberInput)
+    EditText registerPhoneNumberInput;
+
+    @BindView(R.id.registerCNICInput)
+    EditText registerCNICInput;
+
+    @BindView(R.id.registerConfirmPasswordInput)
+    EditText registerConfirmPasswordInput;
+
+    @BindView(R.id.registerAddressInput)
+    EditText registerAddressInput;
+
     FirebaseAuth mAuth;
+    FirebaseDatabase mDatabase;
+    DatabaseReference mReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +61,8 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        mDatabase = FirebaseDatabase.getInstance();
+        mReference = mDatabase.getReference("User");
 
     }
 
@@ -44,14 +71,43 @@ public class CreateAccountActivity extends AppCompatActivity {
         String email = registerEmailInput.getText().toString();
         String password = registerPasswordInput.getText().toString();
 
+        String firstName = registerFirstNameInput.getText().toString();
+        String lastName = registerLastNameInput.getText().toString();
+        String phoneNumber = registerPhoneNumberInput.getText().toString();
+        String cnic = registerCNICInput.getText().toString();
+        String address = registerAddressInput.getText().toString();
+
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
                     FirebaseUser user = mAuth.getCurrentUser();
                     if (user != null){
-                        startActivity(new Intent(CreateAccountActivity.this, HomeActivity.class));
-                        finish();
+
+                        FirebaseDatabaseUser databaseUser = new FirebaseDatabaseUser(
+                                firstName,
+                                lastName,
+                                email,
+                                cnic,
+                                phoneNumber,
+                                address
+                        );
+                        mReference.child(user.getUid()).setValue(databaseUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                                if (task.isSuccessful()){
+                                   Toast.makeText(CreateAccountActivity.this, "Information added successfully!", Toast.LENGTH_LONG).show();
+                                }else {
+                                    Toast.makeText(CreateAccountActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                                }
+
+                                startActivity(new Intent(CreateAccountActivity.this, HomeActivity.class));
+                                finish();
+                            }
+                        });
+
+
                     }else {
                         startActivity(new Intent(CreateAccountActivity.this, MainActivity.class));
                         finish();
---
+++ app/src/main/java/com/dapgarage/lecture1/models/FirebaseDatabaseUser.java
@@ -0,0 +1,42 @@
+package com.dapgarage.lecture1.models;
+
+public class FirebaseDatabaseUser {
+
+    String firstName, lastName, email, cnic, phoneNumber, address;
+
+    public FirebaseDatabaseUser() {
+    }
+
+    public FirebaseDatabaseUser(String firstName, String lastName, String email, String cnic, String phoneNumber, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.cnic = cnic;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCnic() {
+        return cnic;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
